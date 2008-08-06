4c1e80aeb1301e00042d452099e0fda0f9e876f5
Michael Barr <michaeljohnbarr@users.noreply.github.com>
2008-08-06 15:32:39 +0000

    gungame.py: * Changed __version__ to 1.0.441.
    gg_spawn_protect.py:
    * Changed version number to 1.0.441.
    * Fixed a bug to where gg_spawn_protect_cancelonfire would always return as "True".
    * Fixed a bug to where a player firing their weapon would always set their health to 100 each time they fired the weapon.
    * Added player_death and player_disconnect checks.
    * Added additional checks for canceling delays by adding a "list_protected" to store userids that are currently spawn protected.
    * Changed all instances of "gungamelib.getVariable()" to "gungamelib.getVariableValue()".
    
    NOTE: This is a massive bug fix for issues created when re-writing gg_spawn_protect. It has been tested, and seems to work quite well. Please report any issues with this script ASAP.

--- addons/eventscripts/gungame/included_addons/gg_spawn_protect/gg_spawn_protect.py
+++ addons/eventscripts/gungame/included_addons/gg_spawn_protect/gg_spawn_protect.py
@@ -1,7 +1,7 @@
 ''' (c) 2008 by the GunGame Coding Team
 
     Title: gg_spawn_protection
-    Version: 1.0.440
+    Version: 1.0.441
     Description: This will make players invincible and marked with color when
                  ever a player spawns. Protected players cannot level up during
                  spawn protection.
@@ -24,7 +24,7 @@ import gungamelib
 # Register this addon with EventScripts
 info = es.AddonInfo()
 info.name     = 'gg_spawn_protection (for GunGame:Python)'
-info.version  = '1.0.440'
+info.version  = '1.0.441'
 info.url      = 'http://forums.mattie.info/cs/forums/viewforum.php?f=45'
 info.basename = 'gungame/included_addons/gg_spawn_protect'
 info.author   = 'GunGame Development Team'
@@ -33,9 +33,10 @@ info.author   = 'GunGame Development Team'
 #  GLOBALS
 # ==============================================================================
 noisyBefore = 0
+list_protected = []
 
 # ==============================================================================
-#  GAME EVENTS
+#  SCRIPT LOAD/UNLOAD EVENTS
 # ==============================================================================
 def load():
     global noisyBefore
@@ -45,7 +46,7 @@ def load():
     gg_spawn_protect.setDisplayName('GG Spawn Protection')
     
     # Set eventscripts_noisy to 1, and back up the original value
-    if gungamelib.getVariable('gg_spawn_protect_cancelonfire'):
+    if gungamelib.getVariableValue('gg_spawn_protect_cancelonfire'):
         noisyBefore = int(es.ServerVar('eventscripts_noisy'))
         es.ServerVar('eventscripts_noisy').set(1)
     
@@ -56,6 +57,9 @@ def unload():
     # Unregister
     gungamelib.unregisterAddon('gg_spawn_protect')
 
+# ==============================================================================
+#  GAME EVENTS
+# ==============================================================================
 def server_cvar(event_var):
     global noisyBefore
     
@@ -71,18 +75,19 @@ def server_cvar(event_var):
         else:
             # Set noisy back
             es.ServerVar('eventscripts_noisy').set(noisyBefore)
-
+            
 def weapon_fire(event_var):
-    if not gungamelib.getVariable('gg_spawn_protect_cancelonfire'):
+    if not gungamelib.getVariableValue('gg_spawn_protect_cancelonfire'):
         return
         
     userid = int(event_var['userid'])
     
-    # Cancel the delay
-    gamethread.cancelDelayed('ggSpawnProtect%s' %userid)
+    if userid in list_protected:
+        # Cancel the delay
+        gamethread.cancelDelayed('ggSpawnProtect%s' %userid)
     
-    # Fire the end of the spawn protection immediately
-    endProtect(userid)
+        # Fire the end of the spawn protection immediately
+        endProtect(userid)
 
 def player_spawn(event_var):
     # Is a warmup round?
@@ -96,21 +101,39 @@ def player_spawn(event_var):
     if gungamelib.isDead(userid) or gungamelib.isSpectator(userid):
         return
     
+    if userid in list_protected:
+        return
+        
     startProtect(userid)
+    
+def player_death(event_var):
+    userid = int(event_var['userid'])
+    if userid in list_protected:
+        # Cancel the delay (they likely killed themselves if this code was reached)
+        gamethread.cancelDelayed('ggSpawnProtect%s' %userid)
+        list_protected.remove(userid)
+        
+def player_disconnect(event_var):
+    userid = int(event_var['userid'])
+    if userid in list_protected:
+        list_protected.remove(userid)
 
 def startProtect(userid):
     # Retrieve player objects
     playerlibPlayer = playerlib.getPlayer(userid)
     gungamePlayer = gungamelib.getPlayer(userid)
     
+    # Add them to the list of protected players
+    list_protected.append(userid)
+    
     # Set health
     playerlibPlayer.set('health', 999)
     
     # Set color
-    playerlibPlayer.set('color', (gungamelib.getVariable('gg_spawn_protect_red'),
-                                  gungamelib.getVariable('gg_spawn_protect_green'),
-                                  gungamelib.getVariable('gg_spawn_protect_blue'),
-                                  gungamelib.getVariable('gg_spawn_protect_alpha')))
+    playerlibPlayer.set('color', (gungamelib.getVariableValue('gg_spawn_protect_red'),
+                                  gungamelib.getVariableValue('gg_spawn_protect_green'),
+                                  gungamelib.getVariableValue('gg_spawn_protect_blue'),
+                                  gungamelib.getVariableValue('gg_spawn_protect_alpha')))
     
     # Remove hitboxes
     es.setplayerprop(userid, 'CBaseAnimating.m_nHitboxSet', 2)
@@ -126,6 +149,9 @@ def endProtect(userid):
     # Retrieve player objects
     gungamePlayer = gungamelib.getPlayer(userid)
     playerlibPlayer = playerlib.getPlayer(userid)
+    
+    # Remove the player from the list of protected players
+    list_protected.remove(userid)
     
     # Health
     playerlibPlayer.set('health', 100)

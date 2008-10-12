a63cd797c4b9fa7d25f2f1bdef37bba7a2cd4bce
Michael Barr <michaeljohnbarr@users.noreply.github.com>
2008-10-12 17:36:45 +0000

    gungame.py: * Updated __version__ to 1.0.483.
    gg_elimination.py:
    * Updated version to 1.0.483.
    * Changed "userid" to "playerid" in the "gungamelib.respawn()" method located within the "respawnEliminated()" method.
      - Thanks tnbPorsche, for the fast bug report.

--- addons/eventscripts/gungame/gungame.py
+++ addons/eventscripts/gungame/gungame.py
@@ -1,7 +1,7 @@
 ''' (c) 2008 by the GunGame Coding Team
 
     Title: gungame
-    Version: 1.0.482
+    Version: 1.0.483
     Description: The main addon, handles leaders and events.
 '''
 
@@ -27,7 +27,7 @@ from configobj import ConfigObj
 #   ADDON REGISTRATION
 # ==============================================================================
 # Version info
-__version__ = '1.0.482'
+__version__ = '1.0.483'
 es.ServerVar('eventscripts_ggp', __version__).makepublic()
 
 # Register with EventScripts
--- addons/eventscripts/gungame/included_addons/gg_elimination/gg_elimination.py
+++ addons/eventscripts/gungame/included_addons/gg_elimination/gg_elimination.py
@@ -1,7 +1,7 @@
 ''' (c) 2008 by the GunGame Coding Team
 
     Title: gg_elimination
-    Version: 1.0.482
+    Version: 1.0.483
     Description: Players respawn after their killer is killed.
     
     Originally for ES1.3 created by ichthys:
@@ -29,7 +29,7 @@ import gungamelib
 # Register this addon with EventScripts
 info = es.AddonInfo()
 info.name     = 'gg_elimination Addon for GunGame: Python'
-info.version  = '1.0.482'
+info.version  = '1.0.483'
 info.url      = 'http://forums.mattie.info/cs/forums/viewforum.php?f=45'
 info.basename = 'gungame/included_addons/gg_elimination'
 info.author   = 'GunGame Development Team'
@@ -242,7 +242,7 @@ def respawnEliminated(userid, respawnRound):
             continue
         
         # Respawn player
-        gungamelib.respawn(userid)
+        gungamelib.respawn(playerid)
         
         # Add to message format
         players.append('\3%s\1' % gungamelib.getPlayer(playerid).name)

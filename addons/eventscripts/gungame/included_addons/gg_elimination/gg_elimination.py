28071571575339f35515f9e7c444038b5e6293ed
WarrenAlpert <WarrenAlpert@e3089cc8-da40-0410-a224-1b480869dfe5>
2008-10-27 03:45:45 +0000

    gungame.py: * Updated __version__ to 1.0.497.
    gg_elimination.py:
    * Updated version to 1.0.497.
    * Changed the parameter for the "if not respawnable()" condition in the respawnPlayer() method from "playerid" to "userid" as playerid was incorrect.

--- addons/eventscripts/gungame/included_addons/gg_elimination/gg_elimination.py
+++ addons/eventscripts/gungame/included_addons/gg_elimination/gg_elimination.py
@@ -1,7 +1,7 @@
 ''' (c) 2008 by the GunGame Coding Team
 
     Title: gg_elimination
-    Version: 1.0.496
+    Version: 1.0.497
     Description: Players respawn after their killer is killed.
     
     Originally for ES1.3 created by ichthys:
@@ -27,7 +27,7 @@ import gungamelib
 # Register this addon with EventScripts
 info = es.AddonInfo()
 info.name     = 'gg_elimination Addon for GunGame: Python'
-info.version  = '1.0.496'
+info.version  = '1.0.497'
 info.url      = 'http://forums.mattie.info/cs/forums/viewforum.php?f=45'
 info.basename = 'gungame/included_addons/gg_elimination'
 info.author   = 'GunGame Development Team'
@@ -148,7 +148,7 @@ def respawnPlayer(userid, respawnRound):
         return
     
     # Make sure the player is respawnable
-    if not respawnable(playerid):
+    if not respawnable(userid):
         return
         
     index = gungamelib.getPlayer(userid)['index']

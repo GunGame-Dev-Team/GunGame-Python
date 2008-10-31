9bd1195f921dc95a71a9971fbf3e6d449df59a18
saul.rennison <saul.rennison@e3089cc8-da40-0410-a224-1b480869dfe5>
2008-10-31 23:35:52 +0000

    gungame.py:  * Updated version.
    gungame.ini:
     * Reverted #.

--- addons/eventscripts/gungame/gungame.py
+++ addons/eventscripts/gungame/gungame.py
@@ -1,7 +1,7 @@
 ''' (c) 2008 by the GunGame Coding Team
 
     Title: gungame
-    Version: 5.0.506
+    Version: 5.0.507
     Description: The main addon, handles leaders and events.
 '''
 
@@ -27,7 +27,7 @@ from configobj import ConfigObj
 #   ADDON REGISTRATION
 # ==============================================================================
 # Version info
-__version__ = '5.0.506'
+__version__ = '5.0.507'
 es.ServerVar('eventscripts_gg', __version__).makepublic()
 
 # Register with EventScripts

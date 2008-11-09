f97164d14f79568ee69d514c40dea4e7cad752e0
artsemis <artsemis@e3089cc8-da40-0410-a224-1b480869dfe5>
2008-11-09 09:40:05 +0000

    gungame.py - Updated version (Skipped one as it was off in r535)
    gg_en_config.cfg / gg_map_vote
    - Updated a couple missed locations referencing the old directory structure (gungame rather than gungame5)

--- addons/eventscripts/gungame/gungame.py
+++ addons/eventscripts/gungame/gungame.py
@@ -1,7 +1,7 @@
 ''' (c) 2008 by the GunGame Coding Team
 
     Title: gungame
-    Version: 5.0.535
+    Version: 5.0.536
     Description: The main addon, handles leaders and events.
 '''
 
@@ -26,7 +26,7 @@ from configobj import ConfigObj
 #   ADDON REGISTRATION
 # ==============================================================================
 # Version info
-__version__ = '5.0.534'
+__version__ = '5.0.536'
 es.ServerVar('eventscripts_gg', __version__).makepublic()
 
 # Register with EventScripts

5955aa1b22a2ba94a1561c434492afc0346d4085
WarrenAlpert <WarrenAlpert@e3089cc8-da40-0410-a224-1b480869dfe5>
2008-11-11 04:50:15 +0000

    gungame.py: * Updated __version__ to 5.0.539. * Added a check for PreventLevel to stop sounds and multikills from happening.

--- addons/eventscripts/gungame/gungame.py
+++ addons/eventscripts/gungame/gungame.py
@@ -1,7 +1,7 @@
 ''' (c) 2008 by the GunGame Coding Team
 
     Title: gungame
-    Version: 5.0.538
+    Version: 5.0.539
     Description: The main addon, handles leaders and events.
 '''
 
@@ -26,7 +26,7 @@ from configobj import ConfigObj
 #   ADDON REGISTRATION
 # ==============================================================================
 # Version info
-__version__ = '5.0.538'
+__version__ = '5.0.539'
 es.ServerVar('eventscripts_gg', __version__).makepublic()
 
 # Register with EventScripts
@@ -545,6 +545,9 @@ def player_death(event_var):
         if not gungameVictim.isbot and gungamelib.getVariableValue('gg_afk_rounds') > 0:
             afkPunishCheck(userid)
         
+        return
+    
+    if gungameAttacker.getPreventLevel():
         return
     
     # No multikill? Just level up...

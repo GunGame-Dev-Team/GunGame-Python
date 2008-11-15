d225b296bd9857bac181800c2af3c886d07ea426
WarrenAlpert <WarrenAlpert@e3089cc8-da40-0410-a224-1b480869dfe5>
2008-11-15 16:54:19 +0000

    gungame.py: * Updated __version__ to 5.0.546.

--- addons/eventscripts/gungame/gungame.py
+++ addons/eventscripts/gungame/gungame.py
@@ -1,7 +1,7 @@
 ''' (c) 2008 by the GunGame Coding Team
 
     Title: gungame
-    Version: 5.0.544
+    Version: 5.0.546
     Description: The main addon, handles leaders and events.
 '''
 
@@ -26,7 +26,7 @@ from configobj import ConfigObj
 #   ADDON REGISTRATION
 # ==============================================================================
 # Version info
-__version__ = '5.0.545'
+__version__ = '5.0.546'
 es.ServerVar('eventscripts_gg', __version__).makepublic()
 
 # Register with EventScripts

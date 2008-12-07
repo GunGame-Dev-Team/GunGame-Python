7fcddaf444fdd26f4721b0c10f2a132ccb9af5b3
rydeguy <rydeguy@e3089cc8-da40-0410-a224-1b480869dfe5>
2008-12-07 01:07:36 +0000

    gungame.py: -update __version__ to 569.
    gg_nade_bonus.py:
    -revert back to 559.
    NOTE TO SAUL:  If you don't understand what code does ask before you change it.  How could you possibly test it if you don't understand its function?

--- addons/eventscripts/gungame/included_addons/gg_nade_bonus/gg_nade_bonus.py
+++ addons/eventscripts/gungame/included_addons/gg_nade_bonus/gg_nade_bonus.py
@@ -40,51 +40,13 @@ def unload():
     # Unregister this addon with gungamelib
     gungamelib.unregisterAddon('gg_nade_bonus')
 
-
 def player_spawn(event_var):
-    giveBonusWeapons(event_var['userid'])
+    checkBonus(event_var['userid'])
 
 def gg_levelup(event_var):
-    giveBonusWeapons(event_var['attacker'])
+    checkBonus(event_var['attacker'])
 
-def hegrenade_detonate(event_var):
-    # NOTE TO DEVS: Don't know whether I recoded this right... I don't
-    #               understand what it does.
-    
-    userid = event_var['userid']
-    
-    gungamePlayer = gungamelib.getPlayer(userid)
-    if not gungamePlayer.isbot:
-        return
-    
-    if gungamelib.getGlobal('isWarmup'):
-        return
-    
-    if gungamelib.addonRegistered('gg_unl_grenade'):
-        return
-    
-    # Use bonus weapons
-    for weapon in gungamelib.getVariableValue('gg_nade_bonus').split(','):
-        # Prefix with weapon_
-        if not weapon.startswith('weapon_'):
-            weapon = 'weapon_%s' % weapon
-        
-        if weapon == 'weapon_knife':
-            # TODO: [see comment below]
-            continue
-        
-        if weapon[7:] not in gungamelib.getWeaponList('all'):
-            # TODO: Add warning message. Preferably not here but when the value
-            #       is changed in server_cvar?
-            continue
-        
-        # Make them use it
-        es.delayed('0.02', 'es_xsexec %s "use %s"' % (userid, weapon))
-
-# ==============================================================================
-#  HELPER FUNCTIONS
-# ==============================================================================
-def giveBonusWeapons(userid):
+def checkBonus(userid):
     # Int'ify userid
     userid = int(userid)
     
@@ -93,7 +55,6 @@ def giveBonusWeapons(userid):
     
     # Is a spectator or a bot?
     if gungamelib.isSpectator(userid) or gungamePlayer.isbot:
-        # NOTE TO DEVS: Why are we not giving nade bonus weapons to bots?
         return
     
     # If the weapon is a hegrenade, give them the bonus weapon
@@ -111,8 +72,9 @@ def giveBonusWeapons(userid):
             continue
         
         if weapon[7:] not in gungamelib.getWeaponList('all'):
-            # TODO: Add warning message. Preferably not here but when the value
-            #       is changed in server_cvar?
+            print 'Skipping weapon: %s' % weapon
+            # TODO: Add warning message? Preferably not here but when the value
+            #       is changed?
             continue
         
         # Give them the weapon
@@ -120,3 +82,25 @@ def giveBonusWeapons(userid):
     
     # Give it and make them use it
     es.delayed('0.02', 'es_xsexec %s "use weapon_hegrenade"' % userid)
+    
+def hegrenade_detonate(event_var):
+    userid = event_var['userid']
+    
+    gungamePlayer = gungamelib.getPlayer(userid)
+    if not gungamePlayer.isbot:
+        return
+    
+    if gungamelib.getGlobal('isWarmup'):
+        return
+    
+    if gungamelib.addonRegistered('gg_unl_grenade'):
+        return
+    
+    # Get bonus weapon
+    bonusWeapon = gungamelib.getVariableValue('gg_nade_bonus')
+    if 'weapon_' not in bonusWeapon:
+        bonusWeapon = 'weapon_' + bonusWeapon
+    
+    # Give it and make them use it
+    gamethread.delayed(0.01, gungamePlayer.give, (bonusWeapon, 1))
+    es.delayed('0.02', 'es_xsexec %s "use %s"' % (userid, bonusWeapon))
